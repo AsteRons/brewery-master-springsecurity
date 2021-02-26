a4250cb21585c490b03eb30fc460d31e53a0072c
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-02-26 15:19:14 +0100

    Spring Security Default Basic Auth

--- target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-26T11:58:13+0100",
+    date = "2021-02-26T15:13:01+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
 )
 @Component
