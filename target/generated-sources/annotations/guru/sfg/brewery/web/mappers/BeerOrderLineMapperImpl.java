711fa6207070b02b33d730bafb343e7e6c6af174
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-02-26 14:42:46 +0100

    init

---
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.sfg.brewery.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-02-26T11:58:13+0100",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+}
