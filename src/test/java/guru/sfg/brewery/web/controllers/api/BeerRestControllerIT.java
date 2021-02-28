2d80d2f2ead9472bbd080876e443ec03134452d6
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-03-01 00:28:26 +0100

    Spring MVC Path Matchers

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,6 +1,5 @@
 package guru.sfg.brewery.web.controllers.api;
 
-
 import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+/**
+ * Created by jt on 6/13/20.
+ */
 @WebMvcTest
-public class BeerRestControllerTestIT extends BaseIT {
+public class BeerRestControllerIT extends BaseIT {
 
     @Test
     void findBeers() throws Exception{
@@ -21,6 +22,12 @@ public class BeerRestControllerTestIT extends BaseIT {
     @Test
     void findBeerById() throws Exception{
         mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerByUpc() throws Exception{
+        mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
                 .andExpect(status().isOk());
     }
 }
