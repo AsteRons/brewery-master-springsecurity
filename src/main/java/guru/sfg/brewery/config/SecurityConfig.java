153f7bd916049d1473549d39e61e2b7f4e0df713
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-03-06 17:40:40 +0100

    BCrypt Password Encoder Impl

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,27 +20,29 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 
-    PasswordEncoder passwordEncoder() {
-        return new LdapShaPasswordEncoder();
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeRequests(authorize -> {
-                    authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                            .antMatchers("/beers/find", "/beers*").permitAll()
-                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
-                } )
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().and()
-                .httpBasic();
-    }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
 
+            http
+                    .authorizeRequests(authorize -> {
+                        authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                                .antMatchers("/beers/find", "/beers*").permitAll()
+                                .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                                .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+                    } )
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().and()
+                    .httpBasic();
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
             auth.inMemoryAuthentication()
@@ -48,7 +51,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
                     .roles("ADMIN")
                 .and()
                     .withUser("user")
-                    .password("{SSHA}OvyBZH6ADSmmQJNmJYWSrE+z3Yfyz+YoZuz6lg==")
+                    .password("$2a$10$jzR666rh3XglcpyeZetBwu0c4SVEyMyrDqtSg1qlliA2B6HVmwDt.")
                     .roles("USER");
         }
 
