9cc59baa94d283012176edd4533c642ae5d07990
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-03-07 12:41:46 +0100

    Spring Security Configuration

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.RestHeaderAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +13,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -17,26 +23,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 
 
+        public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
+
+            RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+            filter.setAuthenticationManager(authenticationManager);
+            return filter;
+        }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
 
+            http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
+                    UsernamePasswordAuthenticationFilter.class);
+
             http
-                    .authorizeRequests(authorize -> {
-                        authorize
-                                .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                                .antMatchers("/beers/find", "/beers*").permitAll()
-                                .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                                .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
-                    } )
-                    .authorizeRequests()
-                    .anyRequest()
-                    .authenticated()
-                    .and()
-                    .formLogin()
-                    .and()
-                    .httpBasic();
-        }
+                 .authorizeRequests(authorize -> {
+                            authorize
+                                    .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                                    .antMatchers("/beers/find", "/beers*").permitAll()
+                                    .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                                    .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+                        } )
+                        .authorizeRequests()
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .formLogin()
+                        .and()
+                        .httpBasic();
+            }
 
         @Bean
         PasswordEncoder passwordEncoder() {
