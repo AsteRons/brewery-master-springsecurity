3365a315d28205e404500ebba4c85a4846b0c845
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-03-16 15:58:12 +0100

    target

--- target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-14T19:14:52+0100",
+    date = "2021-03-16T15:57:10+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
 )
 @Component
