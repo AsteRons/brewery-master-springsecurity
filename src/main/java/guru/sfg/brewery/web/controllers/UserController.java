821881833f99235ff835241d9ddeb01ffe2c0636
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-04-05 18:03:48 +0200

    Verify 2FA Opt In

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -40,13 +40,30 @@ public class UserController {
         return "user/register2fa";
     }
 
-    private User getUser() {
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
+
 
     @PostMapping
     public String confirm2Fa(@RequestParam Integer verifyCode){
 
-        return "index";
+        User user = getUser();
+
+        log.debug("Entered Code is:" + verifyCode);
+
+        if (googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)) {
+            User savedUser = userRepository.findById(user.getId()).orElseThrow();
+            savedUser.setUserGoogle2fa(true);
+            userRepository.save(savedUser);
+
+            return "/index";
+        } else {
+            // bad code
+            return "user/register2fa";
+        }
     }
+
+    private User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+
 }
