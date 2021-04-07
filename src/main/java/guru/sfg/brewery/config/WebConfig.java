828e5d9c6a0ac9778b6b82e58a905e3a70ac033f
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-04-07 16:47:05 +0200

    Spring MVC CORS Annotation Config

--- src/main/java/guru/sfg/brewery/config/WebConfig.java
+++ src/main/java/guru/sfg/brewery/config/WebConfig.java
@@ -1,15 +1,14 @@
 package guru.sfg.brewery.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        //registry.
+//        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+//    }
 }
